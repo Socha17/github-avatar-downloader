89b525a2949e4eff0615a7d92789463220fc6cc4
Socha17 <mattsocha11@gmail.com>
2017-04-10 20:12:05 +0000

    added url

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,15 @@
 var request = require('request');
 
+var GITHUB_USER = "Socha17";
+var GITHUB_TOKEN = "9265542cbc5b4a4e32379c67f3d3655d8ed8f793";
+
+var repoOwner = 'Socha17';
+var repoName = 'github-avatar-downloader';
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+console.log(requestURL);
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
